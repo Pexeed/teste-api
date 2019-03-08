759d037ca3a9f6aa1d88a1e56599600aa5bef1c6
Daniel Souza <daniel.souza@moovenconsulting.com.br>
2019-03-08 16:50:24 -0300

    Ajuste url das imagens

--- src/main/kotlin/br/com/moovenconsulting/testeapi/model/car/CarBodywork.kt
+++ src/main/kotlin/br/com/moovenconsulting/testeapi/model/car/CarBodywork.kt
@@ -6,7 +6,7 @@ data class CarBodywork (val id: Long, val name: String, val imageUrl: String) {
             return CarBodywork(
                     1L,
                     "Sedã",
-                    "https://localhost:8881/brands/Mercedes/Mercedes@3x.png")
+                    "https://movida-premium-mock.herokuapp.com/brands/Mercedes/Mercedes@3x.png")
         }
     }
 }
