5f3f48d6042d5916f4959d916037b9d460f22074
Daniel Souza <daniel.ss.pexe@gmail.com>
2019-03-12 16:14:49 -0300

    Novas imagens

--- src/main/kotlin/br/com/moovenconsulting/testeapi/Application.kt
+++ src/main/kotlin/br/com/moovenconsulting/testeapi/Application.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class Application
+class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
